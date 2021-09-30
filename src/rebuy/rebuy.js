ea1072c1a7c4121d25efe9136a771510e56be4ef
philipschenk <75783854+philipschenk@users.noreply.github.com>
2021-09-30 17:17:40 +0200

    lower code

--- src/rebuy/rebuy.js
+++ src/rebuy/rebuy.js
@@ -1,4 +1,3 @@
-<script>
 	var items = document.querySelectorAll('.date');
   var dateNum = document.querySelectorAll('.dateNum');
   const dates = [];
@@ -103,38 +102,3 @@ function setRangeFilter() {
 	}
   
 }
-</script>
-<script src="https://cdn.finsweet.com/files/cmslibrary-v1.8.js"></script>
-<script>
-(function() {
-	// create a new Library instance and store it in a variable called "projectsGrid"
-	var projectsGrid = new FsLibrary('.c-news-collection')
-  
-	// define our filter group(s)
-	var myFilters = [
-  {
-  	filterWrapper: ".c-search-parent-multi",
-		filterType: "multi"
-	},
-  {
-		filterWrapper: ".c-filter-wrapper",
-		filterType: "exclusive",
-    filterByClass:".dateNum",
-    filterRange: true
-	}
-	]
-
- // run filter on our instance
-	projectsGrid.filter({
-		filterArray: myFilters, // the filter group name we defined
-    activeClass: 'fltr-active', // the active class we give to our buttons
-    filterReset:'.c-filter-reset', // reset all active filter button  
-		animation: {
-			enable: true,
-			duration: 200,
-			easing: 'ease-out',
-			effects: 'fade translate(0px,20px)'
-			}
-  })
-})();
-</script>
