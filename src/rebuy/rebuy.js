2ddbaba0aac77f8b4112f5a05ed728dcb3cd981f
philipschenk <75783854+philipschenk@users.noreply.github.com>
2021-09-30 16:59:12 +0200

    remove script

--- src/rebuy/rebuy.js
+++ src/rebuy/rebuy.js
@@ -1,4 +1,3 @@
-<script src="https://cdn.finsweet.com/files/cmslibrary-v1.8.js"></script>
 var items = document.querySelectorAll(".date");
 var dateNum = document.querySelectorAll(".dateNum");
 const dates = [];
